27fa3145ea8f091b861bfb330bbbb3249cb8c204
Drew Templeton <templetonag@appstate.edu>
2021-12-10 18:12:53 -0500

    added size buttons + resize function

--- sketchpad.js
+++ sketchpad.js
@@ -1,32 +1,76 @@
 const container = document.querySelector('.container');
 
-const DIM = 50;
-const WIDTH = 700 / DIM;
-const HEIGHT = 700 / DIM;
+let dim = 50;
+let width = 700 / dim;
+let height = width;
 
-for(let i = 0; i < DIM * DIM; i++)
-{
-    const div = document.createElement('div');
-    div.classList.add('square');
-    div.addEventListener('mouseenter', () => {
-        div.classList.toggle('grey-square');
-    })
+addSquares();
 
-    div.style.width = `${WIDTH}px`;
-    div.style.height = `${HEIGHT}px`;
-
-    container.appendChild(div);
+function setDimensions(newDim) {
+    dim = newDim;
+    width = 700 / dim;
+    height = width;
 }
 
-const clearBtn = document.querySelector('.clear');
 
+//add a clear button
+const clearBtn = document.querySelector('.clear');
 clearBtn.addEventListener('click', resetPad);
 
-function resetPad(){
+
+//changes all squares to white
+function resetPad() {
     const greySquare = document.querySelectorAll('.grey-square');
 
-    for(let i = 0; i < greySquare.length; i++)
-    {
+    for (let i = 0; i < greySquare.length; i++) {
         greySquare[i].classList.remove('grey-square');
+    }
+}
+
+
+//set resize buttons change dimensions of pad squares
+const smallBtn = document.querySelector('#small');
+smallBtn.onclick = () => {
+    newPad(100);
+}
+
+const mediumBtn = document.querySelector('#medium');
+mediumBtn.onclick = () => newPad(50);
+
+const largeBtn = document.querySelector('#large');
+largeBtn.onclick = () => newPad(25);
+
+const hugeBtn = document.querySelector('#huge');
+hugeBtn.onclick = () => newPad(10);
+
+
+//remove old squares, sets new dimensions, add new squares
+function newPad(newDim) {
+    removeSquares();
+    setDimensions(newDim);
+    addSquares();
+}
+
+function addSquares () {
+    for (let i = 0; i < dim * dim; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        div.addEventListener('mouseenter', () => {
+            div.classList.toggle('grey-square');
+        })
+
+        div.style.width = `${width}px`;
+        div.style.height = `${height}px`;
+
+        container.appendChild(div);
+    }
+}
+
+//removes square divs from container
+function removeSquares() {
+    const divTrash = document.querySelectorAll('.square');
+
+    for (let i = 0; i < dim * dim; i++) {
+        container.removeChild(divTrash[i]);
     }
 }
